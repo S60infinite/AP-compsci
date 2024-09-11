d5a2e5190c5a4139520fa765361cb1c875be4f91
S60infinite <2766407@learn.vsb.bc.ca>
2024-09-11 12:57:21 -0700

    Add files via upload

---
+++ ProblemSet1A/Haiku.java
@@ -0,0 +1,15 @@
+/**
+ * AP Computer Science
+ * Our First Example
+ * By Len Pelletier
+ */
+
+public class Haiku {
+
+    public static void main (String[] args) {   	 
+        //output some text to terminal:
+        System.out.println("I want to sleep");
+        System.out.println("Swat the flies");
+        System.out.println("Softy, please.");
+    }
+}
