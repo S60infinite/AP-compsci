e2c36ec67086b7f4a9c5e97958c8ae9144ee2394
S60infinite <2766407@learn.vsb.bc.ca>
2024-09-11 13:30:53 -0700

    Add files via upload

---
+++ 1B-copy/temperature.java
@@ -0,0 +1,30 @@
+
+/**
+ * Ap Compsci
+ *
+ * MIchael Li
+ * Problem set 1b
+ */
+import java.util.Scanner;
+public class temperature
+{
+    public static void main (String[]args){
+       Scanner input= new Scanner(System.in);
+        System.out.println("Enter a degree in Celsius");
+        double celsius=input.nextDouble();
+       double fahrenheit=celsius*9/5+32;
+       System.out.println(celsius+" celsius is "+fahrenheit+" fahrenheit");
+       
+        
+        
+        
+        
+        
+        
+    }
+}
+        
+    
+    
+    
+ 
